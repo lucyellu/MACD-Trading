e8cf01beef90390eabfaba3528dc31fdb9946c9f
Lucy <lucylu.contact@gmail.com>
2023-06-21 10:49:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -85,11 +85,20 @@ class IntradayMACDAlgorithm(QCAlgorithm):
         shares_to_buy = int(self.GetParameter("shares_to_buy") or 100)
         shares_to_sell = int(self.GetParameter("shares_to_sell") or 100)     
         
+        if holdings <= 2100 and buy_signals >= self.min_buy_signals:
+            self.Buy(self.symbol, shares_to_buy)
+        elif holdings > 0 and sell_signals >= self.min_sell_signals:
+            self.Sell(self.symbol, shares_to_sell)
+#'''
+
+#
+'''
+
+'''
         if holdings <= 200 and buy_signals >= self.min_buy_signals:
             self.SetHoldings(self.symbol, 0.9)
         elif holdings > 0 and sell_signals >= self.min_sell_signals:
             self.SetHoldings(self.symbol, 0)
-
 #'''
 
 
